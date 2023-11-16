93d4fec123ce9e089c9de800a732808e70722a07
Diego Santos <132935254+franciscodiegosantos@users.noreply.github.com>
2023-11-16 15:11:09 -0300

    at

--- DESAFIOS/Aula 20 - Projeto Lista de Tarefas/js/controle.js
+++ DESAFIOS/Aula 20 - Projeto Lista de Tarefas/js/controle.js
@@ -1,72 +1,68 @@
-let contador = 0
-let input = document.getElementById('inputTarefa');
-let btnAdd = document.getElementById('btn-add');
-let main = document.getElementById('areaLista')
+let contador = 0;
+let input = document.getElementById("inputTarefa");
+let btnAdd = document.getElementById("btn-add");
+let main = document.getElementById("areaLista");
 
 function addTarefa() {
-    //PEGAR O VALOR DIGITADO NO INPUT
-    let valorInput = input.value;
+  //PEGAR O VALOR DIGITADO NO INPUT
+  let valorInput = input.value;
 
-    //SE NÃO FOR VAZIO, NEM NULO, NEM INDEFINIDO
-    if((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)) {
-        ++contador;
+  //SE NÃO FOR VAZIO, NEM NULO, NEM INDEFINIDO
+  if (valorInput !== "" && valorInput !== null && valorInput !== undefined) {
+    ++contador;
 
-        let novoItem = `<div id="${contador}" class="item">
+    let novoItem = `<div id="${contador}" class="item">
         <div onclick="marcarTarefa(${contador})" class="item-icone">
             <i id="icone_${contador}" class="fa-classic fa-circle"></i>
         </div>
-        
         <div onclick="marcarTarefa(${contador})" class="item-nome">
             ${valorInput}
         </div>
-       
         <div class="item-botao">
-            <button onclick="deletar(${contador})" class="delete"> 
-                <i class="fa-solid fa-trash-can"></i>  Deletar
-            </button>
+            <button onclick="deletar(${contador})" class="delete"><i class="fa-classic fa-trash-can"></i> Deletar</button>
         </div>
     </div>`;
 
     //ADICIONAR NOVO ITEM NO MAIN
     main.innerHTML += novoItem;
-    
-    //ZERAR OS CAMPOS
+
+    //ZERAR OS CAMPINHOS
     input.value = "";
     input.focus();
-    }
+  }
 }
 
 function deletar(id) {
-    var tarefa = document.getElementById(id);
-    tarefa.remove();
+  var tarefa = document.getElementById(id);
+  tarefa.remove();
 }
 
 function marcarTarefa(id) {
-    var item = document.getElementById(id);
-    var classe = item.getAttribute("class");
-    console.log(classe);
+  var item = document.getElementById(id);
+  var classe = item.getAttribute("class");
+  console.log(classe);
 
-    if (classe == "item") {
-        item.classList.add("clicado");
+  if (classe == "item") {
+    item.classList.add("clicado");
 
-        var icone = documente.getElementById("icone_" + id);
-        icone.classList.remove("fa-circle");
-        icone.classList.add("fa-circle-check");
+    var icone = document.getElementById("icone_" + id);
+    icone.classList.remove("circle");
+    icone.classList.add("circle-check");
 
-        item.parentNode.appendChild(item);
-    } else {
-        item.classList.remove("clicado");
+    item.parentNode.appendChild(item);
+  } else {
+    item.classList.remove("clicado");
 
-        var icone = documente.getElementById("icone_" + id);
-        icone.classList.remove("fa-circle-check");
-        icone.classList.add("fa-circle");
-    }
+    var icone = document.getElementById("icone_" + id);
+    icone.classList.remove("circle-check");
+    icone.classList.add("circle");
+  }
 }
 
-input.addEventListener("keyup", function(event) {
-    //SE TECLOU ENTER (13)
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        btnAdd.click();
-    }
-}) 
+input.addEventListener("keyup", function (event) {
+  //SE TECLOU ENTER (13)
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    btnAdd.click();
+  }
+});
