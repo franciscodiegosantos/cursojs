9666b118eba54c22e995a1769d6f4b51d97b6338
Diego Santos <132935254+franciscodiegosantos@users.noreply.github.com>
2023-11-16 12:45:40 -0300

    configurado botão de check

--- DESAFIOS/Aula 20 - Projeto Lista de Tarefas/js/controle.js
+++ DESAFIOS/Aula 20 - Projeto Lista de Tarefas/js/controle.js
@@ -1,3 +1,4 @@
+let contador = 0
 let input = document.getElementById('inputTarefa');
 let btnAdd = document.getElementById('btn-add');
 let main = document.getElementById('areaLista')
@@ -8,23 +9,58 @@ function addTarefa() {
 
     //SE NÃO FOR VAZIO, NEM NULO, NEM INDEFINIDO
     if((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)) {
-        let novoItem = `<div class="item">
-        <div class="item-icone">
-            <i class="fa-classic fa-circle"></i>
+
+        ++contador;
+
+        let novoItem = `<div id="${contador}" class="item">
+        <div onclick="marcarTarefa(${contador})" class="item-icone">
+            <i id="icone_${contador}" class="fa-classic fa-circle"></i>
         </div>
         
-        <div class="item-nome">
-            Teste de tarefa
+        <div onclick="marcarTarefa(${contador})" class="item-nome">
+            ${valorInput}
         </div>
        
         <div class="item-botao">
-            <button class="delete"> 
+            <button onclick="deletar(${contador})" class="delete"> 
                 <i class="fa-solid fa-trash-can"> Deletar</i>
             </button>
         </div>
     </div>`;
 
+    //ADICIONAR NOVO ITEM NO MAIN
     main.innerHTML += novoItem;
     
+    //ZERAR OS CAMPOS
+    input.value = "";
+    input.focus();
     }
 }
+
+function deletar(id) {
+    var tareffa = document.getElementById(id);
+    tareffa.remove();
+}
+
+function marcarTarefa(id) {
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class');
+    console.log(classe);
+
+    if (classe == "item") {
+        item.classList.add('clicado');
+
+        var icone = documente.getElementById('icone_' + id)
+        icone.classList.remove('fa-classic fa-circle');
+        icone.classList.add('fa-classic fa-circle-check');
+
+    }
+}
+
+input.addEventListener("keyup", function(event) {
+    //SE TECLOU ENTER (13)
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        btnAdd.click();
+    }
+}) 
